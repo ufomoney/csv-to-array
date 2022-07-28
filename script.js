40b76bb8477f89dc5609bf7d0bd3e4b4a018c500
Adam Khalguev <halgadam@gmail.com>
2022-07-28 20:06:04 +0300

    2

--- script.js
+++ script.js
@@ -3,16 +3,24 @@ document.getElementById('input').addEventListener('change', event => {
   selectedFile = event.target.files[0];
 });
 
+let show = document.getElementById('show');
+
 document.getElementById('btn').addEventListener('click', () => {
   if (selectedFile) {
     let fileReader = new FileReader();
+    let div = '';
     fileReader.readAsText(selectedFile);
     fileReader.onload = event => {
       const result = event.target.result.trim().split('\n');
       const newArray = result.map(item => {
         const [wallet, amount] = item.split(',');
-        return { wallet, amount: amount * 10 ** 18 };
+        const newAmount = BigInt(amount * 10 ** 18);
+        div += `<div>{<span>wallet: "${wallet}"</span>, <span>amount: ${newAmount}</span>},</div>`;
+
+        return { wallet, amount: newAmount };
       });
+      const elem = div.slice(0, -7) + '</div>';
+      show.insertAdjacentHTML('afterBegin', `[${elem}]`);
       console.log(newArray);
     };
   }

441eb1bb6ec8d2e9a51837e73fbbe9b3346eb7ea
Adam Khalguev <halgadam@gmail.com>
2022-07-28 20:32:24 +0300

    3

--- script.js
+++ script.js
@@ -16,12 +16,18 @@ document.getElementById('btn').addEventListener('click', () => {
         const [wallet, amount] = item.split(',');
         const newAmount = BigInt(amount * 10 ** 18);
         div += `<div>{<span>wallet: "${wallet}"</span>, <span>amount: ${newAmount}</span>},</div>`;
-
         return { wallet, amount: newAmount };
       });
       const elem = div.slice(0, -7) + '</div>';
       show.insertAdjacentHTML('afterBegin', `[${elem}]`);
       console.log(newArray);
+      const btn_copy = document.createElement('button');
+
+      btn_copy.innerText = 'COPY';
+      btn_copy.addEventListener('click', () => {
+        navigator.clipboard.writeText(show.innerText);
+      });
+      show.after(btn_copy);
     };
   }
 });
